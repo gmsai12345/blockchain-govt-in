b0aedb10480dd72bf7f6e33c84cdf74ca12b2515
gmsai12345 <saidhakshan4@gmail.com>
2023-09-23 06:24:18 +0530

    commit

--- pages/[cid].js
+++ pages/[cid].js
@@ -1,104 +1,106 @@
 import Head from 'next/head';
-import React, { useRef, useState, useEffect } from 'react'
+import React, { useRef, useState, useEffect } from 'react';
 import mime from 'mime';
+import { useRouter } from 'next/router';
 
 const GATEWAY_URL = process.env.NEXT_PUBLIC_GATEWAY_URL
-  ? process.env.NEXT_PUBLIC_GATEWAY_URL
-  : "https://gateway.pinata.cloud";
+    ? process.env.NEXT_PUBLIC_GATEWAY_URL
+    : 'https://gateway.pinata.cloud';
 
 const CID = ({ fileData }) => {
-  const [href, setHref] = useState("");
+  const [href, setHref] = useState('');
   const downloadRef = useRef(null);
+  const router = useRouter();
 
   useEffect(() => {
-    if(href) {
+    if (href) {
       downloadRef.current.click();
     }
   }, [href]);
-  
+
   const download = async () => {
-    const res = await fetch(`${GATEWAY_URL}/ipfs/${fileData.ipfs_pin_hash}?download=true`);    
+    const res = await fetch(`${GATEWAY_URL}/ipfs/${fileData.ipfs_pin_hash}?download=true`);
     const extension = mime.getExtension(res.headers.get('content-type'));
     const blob = await res.blob();
 
     const supportsFileSystemAccess =
-      'showSaveFilePicker' in window &&
-      (() => {
-        try {
-          return window.self === window.top;
-        } catch {
-          return false;
-        }
-      })();
+        'showSaveFilePicker' in window &&
+        (() => {
+          try {
+            return window.self === window.top;
+          } catch {
+            return false;
+          }
+        })();
+
     // If the File System Access API is supported…
     if (supportsFileSystemAccess) {
-      try {        
+      try {
         const handle = await showSaveFilePicker({
           suggestedName: `${fileData.ipfs_pin_hash}.${extension}`,
-        });        
+        });
         const writable = await handle.createWritable();
         await writable.write(blob);
         await writable.close();
         return;
-      } catch (err) {        
+      } catch (err) {
         if (err.name !== 'AbortError') {
           console.error(err.name, err.message);
-          const blobUrl = URL.createObjectURL(blob); 
-          setHref(blobUrl);   
+          const blobUrl = URL.createObjectURL(blob);
+          setHref(blobUrl);
         }
       }
     }
-  }
+  };
+
+  const handleButtonClick = () => {
+    // Redirect to the specified URL when the button is clicked
+    router.push('https://ipfs-hash-to-blockchain.vercel.app/');
+  };
 
   return (
-    <>
-      <Head>
-        <title>Simple IPFS</title>
-        <meta name="description" content="Generated with create-pinata-app" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/pinnie.png" />
-      </Head>
-      <main className="m-auto flex min-h-screen w-full flex-col items-center justify-center">
-        <div className="m-auto flex h-full w-full flex-col items-center justify-center bg-cover bg-center">
-          <div className="h-full max-w-screen-xl">
-            <div className="m-auto flex h-full w-full items-center justify-center">
-              <div className="m-auto w-3/4 text-center">
-                <h1>Download file</h1>
-                <p className="mt-2">
-                  Please make sure you trust the source of this link. If you don't know who sent you the link and are unsure what will be downloaded, do not click the download button.
-                </p>
-                <a className="hidden" href={href} ref={downloadRef} download={fileData.originalName} />
-                <div className="mt-8 flex flex-col items-center justify-center  p-2 text-center align-center h-64 w-3/4 m-auto rounded-3xl bg-secondary px-4 py-2 text-light transition-all duration-300 ease-in-out hover:bg-accent hover:text-light">
-                  <h2 className="text-3xl">{fileData.metadata.name}</h2>
-                  <h3 className="mb-8">{fileData.metadata.keyvalues.description}</h3>
-                  <button                    
-                    onClick={download}
-                    className="underline"
-                  >
-                    Download
-                  </button>
+      <>
+        <Head>
+          <title>Simple IPFS</title>
+          <meta name="description" content="Generated with create-pinata-app" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/pinnie.png" />
+        </Head>
+        <main className="m-auto flex min-h-screen w-full flex-col items-center justify-center" style={{ background: "lightblue" }}>
+          <img src={"/logoh.jpeg"} alt="Logo" style={{ width: "150px", marginBottom: "20px" }} />
+          <div className="m-auto flex h-full w-full flex-col items-center justify-center bg-cover bg-center">
+            <div className="h-full max-w-screen-xl">
+              <div className="m-auto flex h-full w-full items-center justify-center">
+                <div className="m-auto w-3/4 text-center">
+                  <h1>Download file</h1>
+                  <p className="mt-2">
+                    Please make sure you trust the source of this link. If you don't know who sent you the link and are unsure what will be downloaded, do not click the download button.
+                  </p>
+                  <a className="hidden" href={href} ref={downloadRef} download={fileData.originalName} />
+                  <div className="mt-8 flex flex-col items-center justify-center  p-2 text-center align-center h-64 w-3/4 m-auto rounded-3xl bg-secondary px-4 py-2 text-light transition-all duration-300 ease-in-out hover:bg-accent hover:text-light">
+                    <h2 className="text-3xl">{fileData.metadata.name}</h2>
+                    <h3 className="mb-8">{fileData.metadata.keyvalues.description}</h3>
+                    <button onClick={handleButtonClick}>Download</button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </main>
-    </>
-  )
-}
+        </main>
+      </>
+  );
+};
 
 export async function getServerSideProps(context) {
-  const pinataSDK = require("@pinata/sdk");
+  const pinataSDK = require('@pinata/sdk');
   const pinata = new pinataSDK({ pinataJWTKey: process.env.PINATA_JWT });
-  // Fetch data from external API    
-  const response = await pinata.pinList(    
-    {
-      hashContains: context.query.cid
-    }
-  );
-  
+  // Fetch data from external API
+  const response = await pinata.pinList({
+    hashContains: context.query.cid,
+  });
+
   const fileData = response.rows[0];
-  return { props: { fileData } }
+  return { props: { fileData } };
 }
 
-export default CID
+export default CID;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { useState, useRef } from "react";
 import Head from "next/head";
 import Files from "@/components/Files";
+import logo from "../public/logo.png"; // Import your logo image
 
 require('dotenv').config();
 
@@ -12,8 +13,10 @@ export default function Home() {
     name: "",
     description: "",
   });
-  
-  const inputFile = useRef(null);const uploadFile = async (e) => {
+
+  const inputFile = useRef(null);
+
+  const uploadFile = async (e) => {
     try {
       e.preventDefault();
       setUploading(true);
@@ -35,11 +38,11 @@ export default function Home() {
     }
   };
 
-const handleChange = (e) => {
+  const handleChange = (e) => {
     setFile(e.target.files[0]);
-};
+  };
 
-const loadRecent = async () => {
+  const loadRecent = async () => {
     try {
       const res = await fetch("/api/files");
       const json = await res.json();
@@ -49,92 +52,103 @@ const loadRecent = async () => {
       alert("trouble loading files");
     }
   };
+
   return (
-    <>
-      <Head>
-        <title>Simple IPFS</title>
-        <meta name="description" content="Generated with create-pinata-app" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/pinnie.png" />
-      </Head>
-      <main className="m-auto flex min-h-screen w-full flex-col items-center justify-center">
-        <div className="m-auto flex h-full w-full flex-col items-center justify-center bg-cover bg-center">
-          <div className="h-full max-w-screen-xl">
-            <div className="m-auto flex h-full w-full items-center justify-center">
-              <div className="m-auto w-3/4 text-center">
-                <h1>IPFS for BHARATSAFE</h1>
-                <p className="mt-2">
-                  IPFS - you can upload a file, get a link, and share
-                  it with anyone who needs to access the file. The link is
-                  permanent, but it will only be shared once.
-                </p>
-                <input
-                  type="file"
-                  id="file"
-                  ref={inputFile}
-                  onChange={handleChange}
-                  style={{ display: "none" }}
-                />
-                <div className="mt-8 flex flex-col items-center justify-center rounded-lg bg-light p-2 text-center text-secondary">
-                  <button
-                    disabled={uploading}
-                    onClick={() => inputFile.current.click()}
-                    className="align-center flex h-64 w-3/4 flex-row items-center justify-center rounded-3xl bg-secondary px-4 py-2 text-light transition-all duration-300 ease-in-out hover:bg-accent hover:text-light">
-                    {uploading ? (
-                      "Uploading..."
-                    ) : (
-                      <div>
-                        <p className="text-lg font-light">
-                          Select a file to upload to the IPFS network
-                        </p>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className="m-auto mt-4 h-12 w-12 text-white">
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5">
-                          </path>
-                        </svg>
-                      </div>
-                    )}
-                  </button>
+      <>
+        <Head>
+          <title>Simple IPFS</title>
+          <meta name="description" content="Generated with create-pinata-app" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/pinnie.png" />
+        </Head>
+        <main className="m-auto flex min-h-screen w-full flex-col items-center justify-center" style={{ background: "lightblue" }}>
+          {/* Add the logo image here */}
+          <img src={"/logoh.jpeg"} alt="Logo" style={{ width: "150px", marginBottom: "20px" }} />
+
+          <div className="m-auto flex h-full w-full flex-col items-center justify-center bg-cover bg-center">
+            <div className="h-full max-w-screen-xl">
+              <div className="m-auto flex h-full w-full items-center justify-center">
+                <div className="m-auto w-3/4 text-center">
+                  <h1>IPFS for BHARATSAFE</h1>
+                  <p className="mt-2">
+                    IPFS - you can upload a file, get a link, and share
+                    it with anyone who needs to access the file. The link is
+                    permanent, but it will only be shared once.
+                  </p>
+                  <input
+                      type="file"
+                      id="file"
+                      ref={inputFile}
+                      onChange={handleChange}
+                      style={{ display: "none" }}
+                  />
+                  <div className="mt-8 flex flex-col items-center justify-center rounded-lg bg-light p-2 text-center text-secondary">
+                    <button
+                        disabled={uploading}
+                        onClick={() => inputFile.current.click()}
+                        className="align-center flex h-64 w-3/4 flex-row items-center justify-center rounded-3xl bg-secondary px-4 py-2 text-light transition-all duration-300 ease-in-out hover:bg-accent hover:text-light"
+                    >
+                      {uploading ? (
+                          "Uploading..."
+                      ) : (
+                          <div>
+                            <p className="text-lg font-light">
+                              Select a file to upload to the IPFS network
+                            </p>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="m-auto mt-4 h-12 w-12 text-white"
+                            >
+                              <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+                              />
+                            </svg>
+                          </div>
+                      )}
+                    </button>
+                  </div>
+                  {file && (
+                      <form onSubmit={uploadFile}>
+                        <div className="mb-2">
+                          <label htmlFor="name">Name</label><br/>
+                          <input
+                              onChange={(e) => setForm({
+                                ...form,
+                                name: e.target.value
+                              })}
+                              className="border border-secondary rounded-md p-2 outline-none"
+                              id="name"
+                              value={form.name}
+                              placeholder="Name"
+                          />
+                        </div>
+                        <div>
+                          <label htmlFor="description">Description</label><br />
+                          <textarea
+                              className="border border-secondary rounded-md p-2 outline-none"
+                              value={form.description}
+                              onChange={(e) => setForm({
+                                ...form,
+                                description: e.target.value
+                              })}
+                              placeholder="Description..."
+                          />
+                        </div>
+                        <button className="rounded-lg bg-secondary text-white w-auto p-4" type="submit">Upload</button>
+                      </form>
+                  )}
+                  {cid && <Files cid={cid} />}
                 </div>
-                {file && (
-                  <form onSubmit={uploadFile}>
-                    <div className="mb-2">
-                      <label htmlForm="name">Name</label><br/>
-                      <input onChange={(e) => setForm({
-                        ...form, 
-                        name: e.target.value
-                      })} className="border border-secondary rounded-md p-2 outline-none" id="name" value={form.name} placeholder="Name" />
-                    </div>
-                    <div>
-                      <label htmlForm="description">Description</label><br />
-                      <textarea
-                        className="border border-secondary rounded-md p-2 outline-none"
-                        value={form.description}
-                        onChange={(e) => setForm({
-                          ...form, 
-                          description: e.target.value
-                        })}
-                        placeholder="Description..."
-                      />                      
-                    </div>
-                    <button className="rounded-lg bg-secondary text-white w-auto p-4" type="submit">Upload</button>
-                  </form>
-                )}
-                {cid && <Files cid={cid} />}
-                
               </div>
             </div>
           </div>
-        </div>
-      </main>
-    </>
+        </main>
+      </>
   );
 }
